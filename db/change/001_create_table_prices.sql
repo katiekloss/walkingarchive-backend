3c3d5ea4051517009c79a7fe951e0dbfa718a12d
Katie Kloss <kloss.katherine@gmail.com>
2013-03-18 12:20:37 -0400

    Add Prices table to schema

---
+++ db/change/001_create_table_prices.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE Prices (
+	cardid integer NOT NULL,
+	setid integer NOT NULL,
+	price money NOT NULL,
+	source text,
+
+	PRIMARY KEY (cardid, setid),
+	FOREIGN KEY (cardid) REFERENCES Cards ON DELETE CASCADE,
+	FOREIGN KEY (setid) REFERENCES Sets ON DELETE CASCADE
+);
+ALTER TABLE Prices OWNER TO walkingarchive;
+
+INSERT INTO MetaSchemaHistory (Version, Name, Role, Date) VALUES (1, '001_create_table_prices.sql', current_user, current_timestamp);
--- db/schema.sql
+++ db/schema.sql
@@ -117,6 +117,17 @@ CREATE TABLE TradeCards (
 	FOREIGN KEY (cardid) REFERENCES Cards ON DELETE CASCADE
 );
 
+CREATE TABLE Prices (
+	cardid integer NOT NULL,
+	setid integer NOT NULL,
+	price money NOT NULL,
+	source text,
+
+	PRIMARY KEY (cardid, setid),
+	FOREIGN KEY (cardid) REFERENCES Cards ON DELETE CASCADE,
+	FOREIGN KEY (setid) REFERENCES Sets ON DELETE CASCADE
+);
+
 ---
 --- Views
 ---
