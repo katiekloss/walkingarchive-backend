16e91aa342f313038775eccbec084ff6dbdf61ff
Katie Kloss <kloss.katherine@gmail.com>
2013-03-21 22:13:21 -0400

    Update JUnit version and add example test

---
+++ src/test/java/org/walkingarchive/backend/ExampleTest.java
@@ -0,0 +1,14 @@
+package org.walkingarchive.backend;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class ExampleTest
+{
+
+	@Test
+	public void oneEqualsOne()
+	{
+		Assert.assertEquals(1, 1);
+	}
+}
