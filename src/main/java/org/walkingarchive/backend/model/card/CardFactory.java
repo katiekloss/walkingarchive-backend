16a3144e04c372e77107ec58e70428971676eda6
Katie Kloss <kloss.katherine@gmail.com>
2013-03-24 20:40:41 -0400

    Make getCardsByName case-insensitive

--- src/main/java/org/walkingarchive/backend/model/card/CardFactory.java
+++ src/main/java/org/walkingarchive/backend/model/card/CardFactory.java
@@ -34,7 +34,7 @@ public class CardFactory {
     
     public List<Card> getCardsByName(String name) {
         Session session = DbHelper.getSession();
-        List cards = session.createQuery("from Card where lower(name) like :name")
+        List cards = session.createQuery("from Card where lower(name) like lower(:name)")
             .setParameter("name", name)
             .list();
         return cards;
