0f329b5004b632bec01e03c982f1292e4273c77b
Katie Kloss <klk103@case.edu>
2013-04-24 18:10:36 +0000

    Separate ts_rank_intersect and ts_rank_idf

--- extensions/ts_rank_intersect/ts_rank_intersect.c
+++ extensions/ts_rank_intersect/ts_rank_intersect.c
@@ -10,10 +10,13 @@ PG_MODULE_MAGIC;
 #endif
 
 PG_FUNCTION_INFO_V1(ts_rank_intersect);
+PG_FUNCTION_INFO_V1(ts_rank_idf);
 
 int tokenize(char *, char ***);
-float getTokenIDF(char *);
+long getTokenFrequency(char *);
+long getDocumentCount(void);
 Datum ts_rank_intersect(PG_FUNCTION_ARGS);
+Datum ts_rank_idf(PG_FUNCTION_ARGS);
 
 int tokenize(char *string, char ***outArray)
 {
@@ -37,15 +40,15 @@ int tokenize(char *string, char ***outArray)
         return tokens;
 }
 
-float getTokenIDF(char *token)
+long getTokenFrequency(char *token)
 {
     bool isNull;
     text *tokenTextP;
-    Datum tokenDatum, idfDatum;
+    Datum tokenDatum, countDatum;
     Datum *args;
     Oid *argTypes;
     int spiResultCode;
-    float idf;
+    long count;
 
     SPI_connect();
     tokenTextP = cstring_to_text(token);
@@ -55,8 +58,10 @@ float getTokenIDF(char *token)
     args = (Datum *) palloc(sizeof(Datum));
     args[0] = tokenDatum;
 
+    count = -1;
+
     spiResultCode = SPI_execute_with_args(
-        "SELECT idf::real FROM IDF WHERE token = $1",
+        "SELECT count::integer FROM TokenDictionaryMaterialized WHERE token = $1",
         1,
         argTypes,
         args,
@@ -64,13 +69,18 @@ float getTokenIDF(char *token)
         true,
         1
     );
+
     if(spiResultCode == SPI_OK_SELECT)
     {
-        idfDatum = SPI_getbinval(SPI_tuptable->vals[0], SPI_tuptable->tupdesc, 1, &isNull);
-        idf = DatumGetFloat4(idfDatum);
+        if(SPI_processed > 0)
+        {
+            countDatum = SPI_getbinval(SPI_tuptable->vals[0], SPI_tuptable->tupdesc, 1, &isNull);
+            count = DatumGetInt32(countDatum);
+        } else {
+            ereport(ERROR, (errmsg("Token '%s' is not in TokenDictionaryMaterialized", token)));
+        }
     } else {
         ereport(ERROR, (errmsg("Unable to query TokenDictionaryMaterialized table. Does it exist?")));
-        idf = -1.0;
     }
 
     pfree(args);
@@ -78,49 +88,100 @@ float getTokenIDF(char *token)
     pfree(tokenTextP);
     SPI_finish();
 
-    return idf;
+    return count;
+}
+
+
+long getDocumentCount()
+{
+    int spiResultCode;
+    Datum countDatum;
+    bool isNull;
+    long count;
+
+    SPI_connect();
+    spiResultCode = SPI_execute("SELECT COUNT(*)::integer FROM CardVectors", 1, 1);
+    if(spiResultCode == SPI_OK_SELECT)
+    {
+        countDatum = SPI_getbinval(SPI_tuptable->vals[0], SPI_tuptable->tupdesc, 1, &isNull);
+        count = DatumGetInt32(countDatum);
+    } else {
+        ereport(ERROR, (errmsg("Unable to query CardVectors table. Does it exist?")));
+        count = -1;
+    }
+
+    SPI_finish();
+    return count;
 }
 
 Datum ts_rank_intersect(PG_FUNCTION_ARGS)
 {
-	char **documentTokenList, **queryTokenList;
-	char *document, *query;
-	int documentWords, queryWords;
-	int i, j;
+    char **documentTokenList, **queryTokenList;
+    char *document, *query;
+    int documentWords, queryWords;
+    int i, j, score;
+
+    document = text_to_cstring(PG_GETARG_TEXT_P(0));
+    query = text_to_cstring(PG_GETARG_TEXT_P(1));
+
+    documentWords = tokenize(document, &documentTokenList);
+    queryWords = tokenize(query, &queryTokenList);
+    score = 0;
+    for(i = 0; i < queryWords; i++)
+    {
+        for(j = 0; j < documentWords; j++)
+        {
+            if(!strcmp(queryTokenList[i], documentTokenList[j]))
+                score++;
+        }
+    }
+
+    pfree(document);
+    pfree(query);
+    for(i = 0; i < queryWords; i++)
+        pfree(queryTokenList[i]);
+    for(i = 0; i < documentWords; i++)
+        pfree(documentTokenList[i]);
+    pfree(queryTokenList);
+    pfree(documentTokenList);
+
+    PG_RETURN_INT32(score);
+}
+
+Datum ts_rank_idf(PG_FUNCTION_ARGS)
+{
+    char **documentTokenList, **queryTokenList;
+    char *document, *query;
+    int documentWords, queryWords;
+    int i, j;
+    long documentCount;
     float score;
 
-	document = text_to_cstring(PG_GETARG_TEXT_P(0));
-	query = text_to_cstring(PG_GETARG_TEXT_P(1));
+    document = text_to_cstring(PG_GETARG_TEXT_P(0));
+    query = text_to_cstring(PG_GETARG_TEXT_P(1));
 
-	documentWords = tokenize(document, &documentTokenList);
-	queryWords = tokenize(query, &queryTokenList);
-
-    score = 0.0;
-	for(i = 0; i < queryWords; i++)
-	{
-		for(j = 0; j < documentWords; j++)
-		{
-			if(!strcmp(queryTokenList[i], documentTokenList[j]))
-			    score += getTokenIDF(queryTokenList[i]);
-		}
-	}
+    documentWords = tokenize(document, &documentTokenList);
+    queryWords = tokenize(query, &queryTokenList);
 
     documentCount = getDocumentCount();
     score = 0.0;
-    for(i = 0; i < intersectionIdx; i++)
+    for(i = 0; i < queryWords; i++)
     {
-        tokenDocumentCount = getTokenFrequency(queryTokenList[intersections[i]]);
-        score += (float) documentCount / tokenDocumentCount;
+        for(j = 0; j < documentWords; j++)
+        {
+            if(!strcmp(queryTokenList[i], documentTokenList[j]))
+			    score += (float) documentCount / getTokenFrequency(queryTokenList[i]);
+        }
     }
 
-	pfree(document);
-	pfree(query);
-	for(i = 0; i < queryWords; i++)
-		pfree(queryTokenList[i]);
-	for(i = 0; i < documentWords; i++)
-		pfree(documentTokenList[i]);
-	pfree(queryTokenList);
-	pfree(documentTokenList);
+    pfree(document);
+    pfree(query);
+    for(i = 0; i < queryWords; i++)
+        pfree(queryTokenList[i]);
+    for(i = 0; i < documentWords; i++)
+        pfree(documentTokenList[i]);
+    pfree(queryTokenList);
+    pfree(documentTokenList);
 
-	PG_RETURN_FLOAT4(score);
+    PG_RETURN_FLOAT4(score);
 }
